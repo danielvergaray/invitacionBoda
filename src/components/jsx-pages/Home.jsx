44b2a6a4bfc5f49b42e3fe9015b1536dc6a9140a
Daniel Vergaray <danielvergarayescobar@gmail.com>
2024-03-15 19:09:04 -0500

    prueba animacion hero

--- src/components/jsx-pages/Home.jsx
+++ src/components/jsx-pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
   return (
     <>
       <section
-        className={`mobile-home-hero ${fondoInicio ? 'primera-imagen' : 'segunda-imagen'}`}
+        className={fondoInicio ? "mobile-home-hero"   : 'segunda-imagen'}
         /* style={{
           backgroundImage: fondoInicio
             ? `url(${imagenHome1})`
